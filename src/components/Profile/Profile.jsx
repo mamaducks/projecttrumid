8440aef2ffad71fec4c93c01c33fe1efc9387593
mamaducks <leonad308@gmail.com>
2021-12-17 18:43:48 -0500

    profile avatar profile badge

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,12 @@
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-// import CardMedia from "@mui/material/CardMedia";
-// import logo from "./logo.svg";
 import Container from "@mui/material/Container";
-// import ProfileData from "./ProfileData";
-import { CardHeader, Paper } from "@mui/material";
-import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import Link from "@mui/material/Link";
+import { Paper } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
-import { profileInfo } from "../../model/profile";
+import { getProfileInfo } from "../../model/profile";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
@@ -17,33 +14,40 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ShieldIcon from "@mui/icons-material/Shield";
 import Badge from "@mui/material/Badge";
-import { Icon } from "@mui/material";
-import BadgeImprove from "../../img/badgeupward.svg";
 import { BadgeCard } from "../../img/BadgeCard";
 import GroupWorkIcon from "@mui/icons-material/GroupWork";
 import Divider from "@mui/material/Divider";
 
-import BadgeAllWays from "../../img/badgearrowslg.png";
-import BadgeFixer from "../../img/badgegear.svg";
-import BadgeImprover from "../../img/badgeup.png";
-import BadgeFast from "../../img/badgefastlg.png";
-import BadgeStar from "../../img/badgesuccess.svg";
+import { useMemo } from "react";
 
 export function Profile() {
   const { profileId } = useParams();
-  const userInfo = useRecoilValue(profileInfo(profileId));
+  const profileInfo = useRecoilValue(getProfileInfo(profileId));
 
-  if (!userInfo) {
+  const badgesWithSrc = useMemo(
+    () =>
+      profileInfo?.badges.map((item) => {
+        const blob = new Blob([item.src], { type: "image/svg+xml" });
+        const url = URL.createObjectURL(blob);
+
+        return { ...item, url };
+      }),
+    [profileInfo?.badges]
+  );
+
+  if (!profileInfo) {
     return <div>User Profile not found</div>;
   }
 
-  const { name, title,  badges = [], projects = [] } = userInfo;
+  // console.log(JSON.stringify(profileInfo, null, 1));
+  console.log(profileInfo);
+
+  const { name, title, badges = [], projects = [] } = profileInfo;
 
   return (
     <Box sx={{ backgroundColor: "#001e4b" }}>
       <Container>
         <Paper square={true}>
-          ID = {JSON.stringify(userInfo, null, 3)}
           <div>
             <Card>
               {/* <CardMedia
@@ -69,166 +73,25 @@ export function Profile() {
                     <Box my={1} typography="subtitle1" alignSelf="center">
                       Badges
                       <Badge badgeContent={2} color="secondary">
-                        <ShieldIcon color="primary" alignSelf="center"/>
+                        <ShieldIcon color="primary" alignSelf="center" />
                       </Badge>
                     </Box>
                   </Box>
                 </Box>
 
-                {/* <Box
-              sx={{
-                display: "flex",
-                flexGrow: 1,
-                justifyContent: "space-between",
-              }}
-            >
-              {badges.map((item) => (
-                <Box title={item.title}>
-                  <ShieldIcon fontSize="large" />
-                  <Icon>
-                    <img
-                      src={BadgeImprove}
-                      alt="shield"
-                      height={25}
-                      width={25}
-                    />{" "}
-                  </Icon>
-                  <Box sx={{ typography: "subtitle3" }}>{item.name}</Box>
-
-                  <Box sx={{ typography: "subtitle1" }}>{item.desc}</Box>
-                </Box>
-              ))}
-            </Box> */}
-                {/* <br />
-                <br />
-                <br /> */}
-
-                {/* <Box
-                  sx={{ width: "100%", textAlign: "center", typography: "h5" }}
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexGrow: 1,
+                    justifyContent: "space-between",
+                  }}
                 >
-                  Badges
-                </Box>
-                <br />
-                <Divider />
-
-                <Box display="flex">
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      px: 4,
-                      pt: 5,
-                    }}
-                    title="mouse over"
-                  >
-                    <Box sx={{ pb: 4 }}>Improved</Box>
-                    <img
-                      src={BadgeImprove}
-                      alt="shield"
-                      height={70}
-                      width={70}
-                    />
-                  </Box>
-
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      px: 4,
-                      pt: 5,
-                    }}
-                    title="mouse over"
-                  >
-                    <Box sx={{ pb: 4 }}> Helpful</Box>
-                    <img
-                      src={BadgeAllWays}
-                      alt="shield"
-                      height={70}
-                      width={70}
-                    />
-                  </Box>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      px: 4,
-                      pt: 5,
-                    }}
-                    title="mouse over"
-                  >
-                    <Box sx={{ pb: 4 }}>Fixer</Box>
-
-                    <img src={BadgeFixer} alt="shield" height={70} width={70} />
-                  </Box>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      px: 4,
-                      pt: 5,
-                    }}
-                    title="mouse over"
-                  >
-                    <Box sx={{ pb: 4 }}>Improvepng</Box>
-
-                    <img
-                      src={BadgeImprover}
-                      alt="shield"
-                      height={70}
-                      width={70}
-                    />
-                  </Box>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      px: 4,
-                      pt: 5,
-                    }}
-                    title="mouse over"
-                  >
-                    <Box sx={{ pb: 4 }}>Fast</Box>
-
-                    <img src={BadgeFast} alt="shield" height={70} width={70} />
-                  </Box> */}
-                
-                
-
-                {/* </Box> 
-
-                 <div>Missions</div>
-
-            <Box
-              sx={{
-                display: "flex",
-                flexGrow: 1,
-                justifyContent: "space-between",
-              }}
-            >
-              {projects.map((item) => (
-                <Box title={item.title}>
-                  <Box sx={{ typography: "subtitle3" }}>{item.name}</Box>
-
-                  <Box sx={{ typography: "subtitle1" }}>{item.desc}</Box>
-
-                  <div>----</div>
-
-                  {item.roles.map((role) => (
-                    <Box sx={{ typography: "subtitle3" }}>{role.name}</Box>
+                  {badgesWithSrc.map((item) => (
+                    <Box title={item.title}>
+                      <img src={item.url} alt="shield" height={70} width={70} />
+                    </Box>
                   ))}
                 </Box>
-              ))}
-            </Box> */}
               </CardContent>
             </Card>
           </div>
@@ -236,17 +99,25 @@ export function Profile() {
           <Card sx={{ flexDirection: "column", pb: 10 }}>
             <Box textAlign="center" pb={3} typography="h5">
               Missions
-              <Divider sx={{ml: 12, mr: 12}}/>
-              <br/>
+              <Divider sx={{ ml: 12, mr: 12 }} />
+              <br />
             </Box>
             {projects.map((item) => (
               <Accordion>
                 <AccordionSummary
-                  expandIcon={<ExpandMoreIcon color="secondary"/>}
+                  expandIcon={<ExpandMoreIcon color="secondary" />}
                   aria-controls="panel1a-content"
                   id="panel1a-header"
                 >
-                  <GroupWorkIcon sx={{ px: 4, alignSelf: "center", color: "#001e4b" }} />
+                  <Link
+                    href={`/project/${item.id}`}
+                    underline="hover"
+                    color="#000"
+                  >
+                    <GroupWorkIcon
+                      sx={{ px: 4, alignSelf: "center", color: "#001e4b" }}
+                    />
+                  </Link>
 
                   <Box
                     sx={{ typography: "subtitle1", alignItems: "center" }}
@@ -270,8 +141,9 @@ export function Profile() {
                       >
                         {role.name}
                       </Typography>
+
                       <Typography variant="subtitle1" textAlign="right">
-                        {item.desc}
+                        {role.desc}
                       </Typography>
                     </Box>
                   </AccordionDetails>
@@ -279,7 +151,6 @@ export function Profile() {
               </Accordion>
             ))}
           </Card>
-          {/* <ProfileData /> */}
         </Paper>
       </Container>
     </Box>
---
+++ src/components/Profile/ProfileAvatar.jsx
@@ -0,0 +1,32 @@
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
+import { useRecoilValue } from "recoil";
+import { getProfileInfo } from "../../model/profile";
+
+export function ProfileAvatar({ profileId }) {
+  const profileInfo = useRecoilValue(getProfileInfo(profileId));
+
+  if (!profileInfo) {
+    return null;
+  }
+
+  const { projects, badges } = profileInfo;
+
+  return (
+    <Badge
+      overlap="circular"
+      anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+      color="primary"
+      badgeContent={projects.length}
+    >
+      <Badge
+        overlap="circular"
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        color="secondary"
+        badgeContent={badges.length}
+      >
+        <Avatar alt={profileInfo.name} src="/static/images/avatar/2.jpg" />
+      </Badge>
+    </Badge>
+  );
+}
